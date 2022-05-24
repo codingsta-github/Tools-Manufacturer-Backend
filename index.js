589cf402e2fb9251559ed80ab44601228497bece
coding-sta <personal.sta.bd@gmail.com>
2022-05-24 15:58:33 +0600

    single item

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { query } = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
@@ -19,27 +20,36 @@ async function run() {
     client.connect();
     const toolsCollection = client.db("tools-manufacturer").collection("tools");
     const usersCollection = client.db("tools-manufacturer").collection("users");
-    
-    
+
     app.get("/tools", async (req, res) => {
       const query = {};
       const cursor = toolsCollection.find(query);
       const results = await cursor.toArray();
-      res.send(results)
+      res.send(results);
     });
 
-app.put('/user/:email',async(req,res)=>{
-  const email=req.params.email
-  const user=req.body
-  const filter={email:email}
-  const options={upsert:true}
-  const updateDoc={
-    $set:user,
-  }
-  const results=await usersCollection.updateOne(filter,updateDoc,options)
-  res.send(results)
-})
+    app.get("/tool/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result =await toolsCollection.findOne(query);    
+      res.send(result);
+    });
 
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const results = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(results);
+    });
   } finally {
   }
 }
